8be0f1d5ff1df6a6e52b4a283f693431daf2f66a
icecrash <icecrash@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-02-04 19:26:21 +0000

    added lib directory
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sk_calendar/trunk@1970 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ lib/storagebackends/class.storagebackend_typo3.php
@@ -0,0 +1,39 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2003 Volker Biberger (info@sitekick.de)
+*  All rights reserved
+*
+*  This script is part of the Typo3 project. The Typo3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+* Plugin 'VCE Calendar' for the 'sk_calendar' extension.
+*
+* @author	Volker Biberger <info@sitekick.de>
+*/
+
+class storagebackend_typo3 extends istoragebackend {
+
+	/**
+	 * 
+	 */
+	function storagebackend_typo3() {
+		
+	}	
+}
+?>

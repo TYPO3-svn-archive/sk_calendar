970bb24864dd2bfa71605f268209a1e489da9569
icecrash <icecrash@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-02-07 11:14:54 +0000

    added code for template-inclusion
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sk_calendar/trunk@1973 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -1,5 +1,9 @@
 <?php
+/*
+ * $Id$
+ */
 if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
+t3lib_div::loadTCA("tt_content");
 
 
 if (TYPO3_MODE=="BE")	{
@@ -145,11 +149,12 @@ $TCA["tx_skcalendar_exeptions"] = Array (
 );
 
 
-t3lib_div::loadTCA("tt_content");
 $TCA["tt_content"]["types"]["list"]["subtypes_excludelist"][$_EXTKEY."_pi1"]="layout,select_key";
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_flexform';
+t3lib_extMgm::addPlugin(Array("LLL:EXT:sk_calendar/locallang_db.php:tt_content.list_type", $_EXTKEY."_pi1"),"list_type");
+
 t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:sk_calendar/flexform_ds.xml');
 
+t3lib_extMgm::addStaticFile($_EXTKEY,'static/ts_new/','CSS-based template');
 
-t3lib_extMgm::addPlugin(Array("LLL:EXT:sk_calendar/locallang_db.php:tt_content.list_type", $_EXTKEY."_pi1"),"list_type");
 ?>
